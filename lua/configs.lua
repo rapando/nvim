212b2f9011528c4285606dd1d25a9d1a63b40f90
rapando <samsonrapando@gmail.com>
2025-03-27 19:29:33 +0300

    added outline

--- lua/configs.lua
+++ lua/configs.lua
@@ -459,3 +459,56 @@ vim.keymap.set("n", "<leader>dx", function() dap.step_out() end, { desc = "Step 
 vim.keymap.set("n", "<Leader>b", function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
 vim.keymap.set("n", "<Leader>dr", function() dap.repl.open() end, { desc = "Open Debug Console" })
 vim.keymap.set("n", "<Leader>du", function() dapui.toggle() end, { desc = "Toggle DAP UI" })
+
+-- ************** CONFIGS
+-- trouble
+require("trouble").setup()
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { desc = "Toggle Trouble" })
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { desc = "Workspace Diagnostics" })
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { desc = "Document Diagnostics" })
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { desc = "Quickfix List" })
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { desc = "Location List" })
+
+-- symbol separator
+require("nvim-navic").setup({
+  highlight = true,
+  separator = " > ",
+})
+
+
+local navic = require("nvim-navic")
+require("lspconfig").gopls.setup({
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentSymbolProvider then
+      navic.attach(client, bufnr)
+    end
+  end,
+})
+
+-- show breadcrumbs on statusline
+require("lualine").setup({
+	options = {
+		theme = "auto",
+		globalstatus = true,
+	},
+	sections = {
+		lualine_c = {
+		{ "filename" },
+		{ require("nvim-navic").get_location, cond = require("nvim-navic").is_available },
+    },
+  },
+})
+
+--  code outliner.
+require("aerial").setup({
+  backends = { "lsp", "treesitter", "markdown" },
+  show_guides = true,
+})
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", { desc = "Toggle Aerial (Code Outline)" })
+
+-- todo comments.
+require("todo-comments").setup()
+vim.keymap.set("n", "<leader>t", "<cmd>TodoTelescope<CR>", { desc = "Search TODOs" })
+vim.keymap.set("n", "<leader>xt", "<cmd>TodoTrouble<CR>", { desc = "Show TODOs in Trouble" })
+
+
--- lua/plugins/outline.plugins.lua
+++ lua/plugins/outline.plugins.lua
@@ -7,5 +7,35 @@ return {
                     width = 15,
                 })
         end,
-    }
+    },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies=  {
+			"nvim-tree/nvim-web-devicons"
+		}
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons"
+		}
+	},
+	{
+		"SmiteshP/nvim-navic",
+		dependencies = {
+			"neovim/nvim-lspconfig"
+		}
+	},
+	{
+		"stevearc/aerial.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter"
+		}
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim"
+		}
+	}
 }
