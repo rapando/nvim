3f9589366f98764f400c4f6c39ea8aa5945f0e97
rapando <samsonrapando@gmail.com>
2025-03-27 19:11:00 +0300

    added command completion

--- lua/plugins/floating.plugins.lua
+++ lua/plugins/floating.plugins.lua
@@ -7,13 +7,30 @@ return {
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"rcarriga/nvim-notify",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-cmdline", 
 		},
 		config =  function()
+			
 			require("noice").setup({
 				cmdline = {
 					view = "cmdline_popup",
 				},
+				views = {
+				cmdline_popup = {
+				  position = {
+					row = 10,  -- Adjust vertical position (higher = moves down)
+					col = "50%",  -- Centered horizontally
+				  },
+				  size = {
+					width = 60,  -- Set width of popup
+					height = "auto",
+				  },
+				},
+			  },
 			})
+
+			
 		end
     },
     {
