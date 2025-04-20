6b7374eacc8fed46ae52d0053a568edb1950cf3e
rapando <samsonrapando@gmail.com>
2025-04-20 10:16:44 +0300

    added two rulers

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,7 +4,7 @@ return {
 	{
 		"lukas-reineke/virt-column.nvim",
 		config = function()
-			require("virt-column").setup({ char = "|", virtcolumn = "100" })
+			require("virt-column").setup({ char = ".", virtcolumn = "100,120" })
 		end
 	},
 	-- themes
