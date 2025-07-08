2e2a0b315b0c0b368fc5c88d83e67af9de089919
rapando <samsonrapando@gmail.com>
2025-07-08 22:05:11 +0300

    added M-o

--- lua/general/keybindings.lua
+++ lua/general/keybindings.lua
@@ -6,3 +6,7 @@ vim.api.nvim_set_keymap('n', '<leader>r', ':redraw!<CR>', { noremap = true, sile
 -- manual formatting
 vim.keymap.set('n', 'gq', 'gq', { noremap = true, silent = true })
 
+
+-- map M-o to C-o  because it's also a zellij shortcut.
+vim.keymap.set('n', '<M-o>', '<C-o>', { noremap = true, silent = true })
+
