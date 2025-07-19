279cbfb84a547cc3b95192845e22ac1a804a932b
rapando <samsonrapando@gmail.com>
2025-07-19 23:01:40 +0300

    changes

--- lua/general/keybindings.lua
+++ lua/general/keybindings.lua
@@ -5,3 +5,18 @@ vim.api.nvim_set_keymap('n', '<leader>r', ':redraw!<CR>', { noremap = true, sile
 
 -- manual formatting
 vim.keymap.set('n', 'gq', 'gq', { noremap = true, silent = true })
+
+-- TABS Settings
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Tabs
+map("n", "tt", ":tabnew<CR>", opts)
+map("n", "tn", ":tabnext<CR>", opts)
+map("n", "tp", ":tabprevious<CR>", opts)
+map("n", "tx", ":tabclose<CR>", opts)
+
+-- Tab finder using Telescope
+map("n", "tf", function()
+    require("telescope.builtin").tabs()
+end, { desc = "Find Tab" })
